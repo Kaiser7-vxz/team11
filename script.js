58c89aa93940de16e2420450ed8c5a0680499328
kishan-964 <computervictus5@gmail.com>
2024-12-19 11:55:14 +0545

    index page added

---
+++ script.js
@@ -0,0 +1,7 @@
+// This JavaScript is for handling the search bar functionality (you can expand this if needed)
+
+document.getElementById('search').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    console.log('Searching for:', query);
+    // You can add functionality to filter content based on the search query.
+});
